3864933d606c9d715ad0e10c028dc6505e651a1b
Marcel_Garczyk <marcel.garczyk@pit.lukasiewicz.gov.pl>
2023-11-16 14:36:52 +0100

    Script for remote acces into generator.

---
+++ generator.py
@@ -0,0 +1,46 @@
+from RsSmw import *
+import json
+
+try:
+    with open ("config.json") as config_f:
+        RsSmw.assert_minimum_version('5.0.44')
+        config = json.load(config_f)
+        IP_ADDRESS_GENERATOR = config["IP_ADDRESS_GENERATOR"]
+        PORT = config["PORT"]
+        CONNECTION_TYPE = config["CONNECTION_TYPE"]
+        TRACE_FILE = config["TRACE_FILE"] 
+        MEASURE_TIME = config["MEASURE_TIME"]
+        resource = f'TCPIP::{IP_ADDRESS_GENERATOR}::{PORT}::{CONNECTION_TYPE}'  # Resource string for the device
+        generator = RsSmw(resource, True, True, "SelectVisa='socket'")   
+        config_f.close()
+except FileNotFoundError:
+    print("Brak pliku konfiguracyjnego.")
+    exit()
+
+def com_check():
+    # Driver's instrument status checking ( SYST:ERR? ) after each command (default value is True):
+    generator.utilities.instrument_status_checking = True
+    # The generator object uses the global HW instance one - RF out A
+    generator.repcap_hwInstance_set(repcap.HwInstance.InstA)
+
+
+def meas_prep(set : True, amplitude : int, freq : int):
+    generator.output.state.set_value(set)
+    generator.source.frequency.set_mode(enums.FreqMode.CW)
+    generator.source.power.level.immediate.set_amplitude(amplitude)
+    generator.source.frequency.fixed.set_value(freq)
+    print(f'Channel 1 PEP level: {generator.source.power.get_pep()} dBm')
+
+
+def direct_SCPI():
+    # Direct SCPI interface:
+    response = generator.utilities.query_str('*IDN?')
+    print(f'Direct SCPI response on *IDN?: {response}')
+    generator.close()
+
+
+if __name__ == "__main__":
+    com_check()
+    meas_prep(True, -20, 23E9)
+    direct_SCPI()
+    exit()
