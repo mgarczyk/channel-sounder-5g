6eca99a039234cea1218f87cd1faf0b0cdf9966a
Marcel_Garczyk <marcel.garczyk@pit.lukasiewicz.gov.pl>
2023-11-16 12:57:01 +0100

    Example file for signal generator.

---
+++ R&S_examples/R_S_generator.py
@@ -0,0 +1,23 @@
+from RsSmw import *
+
+RsSmw.assert_minimum_version('5.0.44')
+smw = RsSmw('TCPIP::192.168.0.30::5025::SOCKET', True, True, "SelectVisa=SocketIo")
+
+# Driver's instrument status checking ( SYST:ERR? ) after each command (default value is True):
+smw.utilities.instrument_status_checking = True
+
+# The smw object uses the global HW instance one - RF out A
+smw.repcap_hwInstance_set(repcap.HwInstance.InstA)
+
+
+smw.output.state.set_value(True)
+smw.source.frequency.set_mode(enums.FreqMode.CW)
+smw.source.power.level.immediate.set_amplitude(-20)
+smw.source.frequency.fixed.set_value(23E9)
+print(f'Channel 1 PEP level: {smw.source.power.get_pep()} dBm')
+
+
+# Direct SCPI interface:
+response = smw.utilities.query_str('*IDN?')
+print(f'Direct SCPI response on *IDN?: {response}')
+smw.close()
